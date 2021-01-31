8df9b62d7abb0753eba02c152bf9ac07928e0fd5
cdzdn <floyd9732@gmail.com>
2021-01-31 13:23:23 -0300

    SASS with SMACSS Added

--- functions.php
+++ functions.php
@@ -14,11 +14,26 @@
 
     }
 
+    function load_features(){
+
+        add_theme_support('title-tag');
+
+        register_nav_menu('HeaderMenuLocation','Header Menu Location');
+
+        add_theme_support( 'post-thumbnails', array( 'post', 'page' ) );
+
+        //Remove admin bar
+        remove_action('wp_head', '_admin_bar_bump_cb');
+
+    }
+
+    function load_css(){
+
+        wp_enqueue_style('cssStyle', get_theme_file_uri('style.css'));
+
+    }
+
     function load_javascript() {
-        
-        //O Script esta sendo importado no <head>, porem pode retornar erro se executar antes do DOM carregar totalmente(Ele não encontra os elementos da pagina, pq ainda não foram carregados).
-        //Um solução foi colocar DOMContentLoaded no script, para que só execute quando o DOM carregar. Porem existe outra solução que pode ser feita configurando para que o script seja importado em baixo.
-        //https://wordpress.stackexchange.com/questions/332857/dont-understand-how-to-link-javascript-files-in-wordpress/332914
         
         //Some "static js", without hot loading
 
@@ -35,8 +50,6 @@
         //Another Bootstrap JS
         wp_enqueue_script('bootstrapJS2','//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js');
 
-        wp_enqueue_style('cssStyle', get_theme_file_uri('style.css'));
-
         //Load JivoChat
         wp_enqueue_script('JivoChat','//code.jivosite.com/widget/PvNSwC0cuR');
 
@@ -48,22 +61,16 @@
 
     }
 
-    function load_features(){
-
-        add_theme_support('title-tag');
-
-        register_nav_menu('HeaderMenuLocation','Header Menu Location');
-
-        add_theme_support( 'post-thumbnails', array( 'post', 'page' ) );
-
-    }
+    add_action('wp_enqueue_scripts', 'load_libs');
 
     add_action('after_setup_theme','load_features');
 
-    add_action('wp_enqueue_scripts', 'load_libs');
-
     add_action('wp_enqueue_scripts', 'load_javascript');
 
+    //Active in Production
+    add_action('wp_enqueue_scripts', 'load_css');
+
+    //Deactivate in production
     //add_action('wp_enqueue_scripts','hot_reload');
 
 ?>
